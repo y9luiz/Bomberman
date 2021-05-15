25627639e12632b0cacde0bc2d4d5743609e71de
luiz felipe <y9luiz@hotmail.com>
2021-05-15 02:41:47 -0400

    added circular list to main module

--- Char/char.hpp
+++ Char/char.hpp
@@ -1,13 +1,32 @@
 #pragma once
 #include "../Scenario/Common/renderable_object.hpp"
+#include "../Utils/circular_list.hpp"
 #include <vector>
-#include <memory>
+#include "../Memory/sdl_deleter.hpp"
+typedef  std::vector<std::shared_ptr<SDL_Texture>> sdl_textures_ptr_list;
+
+/**
+ * @brief 
+ *  Four circular buffer of SDL_Textures pointers
+ * 
+ */
+typedef  CircularList<std::shared_ptr<SDL_Texture>> SDL_Texture_CircularList;
+struct DirectionTextures
+{
+    CircularList<std::shared_ptr<SDL_Texture>> up;
+    CircularList<std::shared_ptr<SDL_Texture>> down;
+    CircularList<std::shared_ptr<SDL_Texture>> left;
+    CircularList<std::shared_ptr<SDL_Texture>> rigth;
+};
+
+
 class Character : public RenderableObject {
 
     private:
         //4 types of moviment
         // up,down,left and right
-        std::vector<std::shared_ptr<SDL_Texture>> walk_textures[4];
+        DirectionTextures direction_textures_;
+        sdl_textures_ptr_list walk_textures[4];
         int last_key;
     public:
         Character();
--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <thread>
-
-
 #include "Scenario/defs.hpp"
 #include <Renderer/renderer_window.hpp>
 #include <Scenario/wall_block.hpp>
@@ -21,63 +19,68 @@ int main(int argc, char ** argv)
 {
     try
     {
+        // read the map txt file
         Map scene_map = readMap("../res/map1.txt");
-
+        
         if(SDL_Init(SDL_INIT_VIDEO) > 0)
             throw runtime_error(SDL_GetError());
 
         if(!IMG_Init(IMG_INIT_PNG))
             throw runtime_error(SDL_GetError());
-
+        
+        // create a render object to render the scenario and the objects
         RendererWindow renderer_window("Bomber Man v1",960,480);
 
         bool isRunning = true;
         
         SDL_Event event;
-	std::shared_ptr<SDL_Texture>  texture = std::move(RendererWindow::loadTexture(WALL_IMAGE_PATH));
-	auto  texture2 = std::move(RendererWindow::loadTexture(GROUND_IMAGE_PATH));
-        std::vector<std::shared_ptr<SDL_Texture>> char_texture [4]; 
-        std::shared_ptr<SDL_Texture> texture_ptr;
+        
+        //store the data from the wall blocks into a share_ptr to be placed on many places of the screen
+        std::shared_ptr<SDL_Texture>  texture = std::move(RendererWindow::loadTexture(WALL_IMAGE_PATH));
+        //do the same with the ground blocks
+        auto  texture2 = std::move(RendererWindow::loadTexture(GROUND_IMAGE_PATH));
+        
        
-        /*char_texture[0].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/back_walk6.jpg"));
-        char_texture[0].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/back_walk5.jpg"));
-        char_texture[0].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/back_walk4.jpg"));
-        char_texture[0].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/back_walk3.jpg"));
-        char_texture[0].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/back_walk2.jpg"));
-        char_texture[0].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/back_walk1.jpg"));
+        auto loadTexturesFromCharacter = 
+        /**
+         * @brief Lambda function to read all the input images of the disk and store it into
+         * 
+         * @param texture_folder_path 
+         * @param walk_directions 
+         */
+            [](const std::string texture_folder_path,const std::vector<std::string> walk_directions)
+            {
 
-        char_texture[1].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/front_walk6.jpg"));
-        char_texture[1].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/front_walk5.jpg"));
-        char_texture[1].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/front_walk4.jpg"));
-        char_texture[1].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/front_walk3.jpg"));
-        char_texture[1].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/front_walk2.jpg"));
-        char_texture[1].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/front_walk1.jpg"));
+                int size = walk_directions.size();
 
+                const std::string suffix = ".jpg";
+                const std::string separator = "/";
+
+                std::unique_ptr<SDL_Texture_CircularList[]> char_walk_textures = std::unique_ptr<SDL_Texture_CircularList[]>(new SDL_Texture_CircularList[size]);
+                // a walk_direction name is the direction that character is walking in a string
+                // e.g. back_walk, front_walk
+                int i = 0;
+                for(auto walk_direction:walk_directions)
+                {
+                    std::string texture_filename = texture_folder_path+separator+walk_direction + suffix;
+                    char_walk_textures[i].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/back_walk5.jpg"));
+                    i++;
+                }
+                return char_walk_textures;
+            };  
         
-
-
-        char_texture[2].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/right_walk6.jpg"));
-        char_texture[2].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/right_walk5.jpg"));
-        char_texture[2].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/right_walk4.jpg"));
-        char_texture[2].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/right_walk3.jpg"));
-        char_texture[2].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/right_walk2.jpg"));
-        char_texture[2].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/right_walk1.jpg"));
-
-        char_texture[3].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/left_walk6.jpg"));
-        char_texture[3].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/left_walk5.jpg"));
-        char_texture[3].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/left_walk4.jpg"));
-        char_texture[3].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/left_walk3.jpg"));
-        char_texture[3].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/left_walk2.jpg"));
-        char_texture[3].push_back(RendererWindow::loadTexture("../res/Chars/Bomberman/left_walk1.jpg"));
+        std::vector<std::string> walk_directions = {"back_walk","front_walk","rigth_walk","left_walk"}; 
         
-        */
+        auto  player_textures = loadTexturesFromCharacter("../res/Chars/Bomberman/",walk_directions);
+      
         std::map<char,SDL_Texture*> texture_dict;
         texture_dict.insert(std::pair<char,SDL_Texture*>(BLOCK_WALL,texture.get()));
         texture_dict.insert(std::pair<char,SDL_Texture*>(BLOCK_GROUND,texture2.get()));
-        //texture_dict.insert(std::pair<char,SDL_Texture*>(PLAYER,char_texture[0].back()));
+        //player_textures[0].back();
+	    //texture_dict.insert(std::pair<char,SDL_Texture*>(PLAYER,player_textures[0].));
         //Character p(30,30,32,32,char_texture);
         initializeMapObjectDict(scene_map,texture_dict);
-        //Character *  player = scene_map.getPlayer();
+       //Character *  player = scene_map.getPlayer();
 
         while(isRunning)
         {
@@ -85,20 +88,19 @@ int main(int argc, char ** argv)
             renderer_window.renderMap(scene_map);   
             //renderer_window.render(p);
             renderer_window.display();
-	    std::cout<<"ok\n";
-	    if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-            {
-                isRunning = false;
-            }
-            /*switch( event.type ){
-                case SDL_KEYDOWN:
-                    if( event.key.keysym.sym ){
-                        p.move(event.key.keysym.sym);
-                        event.key.keysym.sym = 0;
-                    }
-                default:
-                    break;
-            }*/
+	        // if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+            // {
+            //     isRunning = false;
+            // }
+            // switch( event.type ){
+            //     case SDL_KEYDOWN:
+            //         if( event.key.keysym.sym ){
+            //             p.move(event.key.keysym.sym);
+            //             event.key.keysym.sym = 0;
+            //         }
+            //     default:
+            //         break;
+            // }
             usleep(10000);
         }
 
@@ -109,8 +111,7 @@ int main(int argc, char ** argv)
     {
         std::cout<<"we have a error\n"<<e.what()<<"\n";
     }
-
-
+    
     
     return 0;
 }
